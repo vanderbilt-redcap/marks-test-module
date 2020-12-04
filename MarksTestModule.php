0c41bca7ded856acb3102ed644c370cb980aeb28
Mark McEver <mark@fastmail.net>
2020-12-04 23:04:52 +0000

    Matched API tokens

--- MarksTestModule.php
+++ MarksTestModule.php
@@ -1,23 +1,26 @@
 <?php namespace Vanderbilt\MarksTestModule;
 
 class MarksTestModule extends \ExternalModules\AbstractExternalModule{
-    function cron(){
-        try{
-            throw new \Exception('
-                Please disregard.  Mark is testing error handling.
-            ');
+    function redcap_every_page_before_render(){
+        if($_SERVER['REQUEST_URI'] !== '/api/'){
+            return;
         }
-        catch(\Throwable $t){
-            $message = str_replace("\n", "<br>", $t->__toString());
-            $this->sendErrorEmail($message);
-        }
-    }
 
-    private function sendErrorEmail($message){
-        $to = 'mark.mcever@vumc.org';
-        $from = $GLOBALS['from_email'];
-        $subject = $this->getModuleName() . ' Module Error';
-        
-        \REDCap::email($to, $from, $subject, $message);
+        $result = $this->query('select api_token from redcap_user_rights where project_id = ? and api_token is not null', 111587);
+        while($row = $result->fetch_assoc()){
+            if($row['api_token'] === $_POST['token']){
+                $logId = $this->log('token match', [
+                    'content' => json_encode($_POST, JSON_PRETTY_PRINT)
+                ]);
+
+                global $from_email;
+                \REDCap::email(
+                    'mark.mcever@vumc.org',
+                    $from_email,
+                    "API Token Match!",
+                    "See log ID $logId for details."
+                );
+            }
+        }
     }
 }
