d6e28f1dc034f4c2b3267231c9576339df06320e
Mark McEver <mark@fastmail.net>
2022-07-05 14:40:02 -0500

    Removed old features no longer needed

--- MarksTestModule.php
+++ MarksTestModule.php
@@ -1,18 +1,5 @@
 <?php namespace Vanderbilt\MarksTestModule;
 
 class MarksTestModule extends \ExternalModules\AbstractExternalModule{
-    function redcap_every_page_before_render(){
-        if($_SERVER['REQUEST_URI'] !== '/api/'){
-            return;
-        }
-
-        $result = $this->query('select api_token from redcap_user_rights where project_id = ? and api_token is not null', 111587);
-        while($row = $result->fetch_assoc()){
-            if($row['api_token'] === $_POST['token']){
-                $logId = $this->log('token match', [
-                    'content' => json_encode($_POST, JSON_PRETTY_PRINT)
-                ]);
-            }
-        }
-    }
+    
 }
