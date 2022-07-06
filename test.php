1d4368af4d458f5fb1ef79395a7f978d19a77643
Mark McEver <mark@fastmail.net>
2022-07-06 12:28:46 -0500

    Moved long running action to ajax call

--- test.php
+++ test.php
@@ -1,5 +1,13 @@
+<h5>Mark's Test Page</h5>
+
 <pre style='white-space: pre-wrap; max-width: 90%'>
-<?php
-var_dump(['return', $module->downloadModule()]);
-?>
+Loading (may take a minute)...
 </pre>
+
+<script>
+    fetch(<?=json_encode($module->getUrl('ajax.php'))?>)
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('#center pre').innerHTML = data
+    })
+</script>
