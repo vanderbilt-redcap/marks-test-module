576fdf593992a0bcae1e1ed54ac8f239f8da37f7
Mark McEver <mark@fastmail.net>
2024-01-23 11:55:12 -0600

    TODO

--- test.php
+++ test.php
@@ -2,6 +2,7 @@
 
 /**
  TODO
+    Add option for type (to show only cron requests for example, like I needed when troubleshooting catmh issue)
     Detect when lines need to roll up under other lines
         Scenario
             All API usage is 20%
